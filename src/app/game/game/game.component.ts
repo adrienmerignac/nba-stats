566ca81d54977eee37edd6e5fbb27532f5476e3d
Adrien MERIGNAC <adrien.merignac@orange.com>
2022-02-22 12:19:12 +0100

    new version of code for game of days

--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -1,47 +1,34 @@
-import {
-  DatePipe
-} from "@angular/common";
-import {
-  Component,
-  OnInit
-} from "@angular/core";
-import {
-  GameInformations
-} from "src/app/@models/game";
-import {
-  NbaService
-} from "src/app/nba.service";
-import {
-  TeamsResponse
-} from "src/app/@models/models";
-import {
-  forkJoin,
-  map,
-} from "rxjs";
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FullInformation, GameInformations } from 'src/app/@models/game';
+import { NbaService } from 'src/app/nba.service';
+import { TeamsResponse } from 'src/app/@models/models';
+import { forkJoin, map } from 'rxjs';
 
 @Component({
-  selector: "app-game",
-  templateUrl: "./game.component.html",
-  styleUrls: ["./game.component.scss"],
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-  gameList!: GameInformations[];
+  gameList!: FullInformation[];
   teamLogo!: TeamsResponse[];
   responsiveOptions;
 
   constructor(private nbaService: NbaService, private datePipe: DatePipe) {
-    this.responsiveOptions = [{
-        breakpoint: "1024px",
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
         numVisible: 3,
         numScroll: 3,
       },
       {
-        breakpoint: "768px",
+        breakpoint: '768px',
         numVisible: 2,
         numScroll: 2,
       },
       {
-        breakpoint: "560px",
+        breakpoint: '560px',
         numVisible: 1,
         numScroll: 1,
       },
@@ -50,83 +37,45 @@ export class GameComponent implements OnInit {
 
   ngOnInit(): void {
     let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
-    let currentDateTime = this.datePipe.transform(yesterday, "yyyy-MMM-dd");
+    let currentDateTime = this.datePipe.transform(yesterday, 'yyyy-MMM-dd');
 
     forkJoin({
-        requestOne: this.nbaService.getMatchsOfTheDay(currentDateTime),
-        requestTwo: this.nbaService.getAllTeams()
-      })
+      requestOne: this.nbaService.getMatchsOfTheDay(currentDateTime),
+      requestTwo: this.nbaService.getAllTeams(),
+    })
       .pipe(
-        map(
-          data => data.requestOne
-          .filter(
-            x => [
-              x.TeamGames
-              .filter(
-                y => y.TeamID
+        map((data) => {
+          const matchOfTheDay = data.requestOne;
+          const allTeams = data.requestTwo;
+
+          let mergeGameInfos = matchOfTheDay.map((infos) => {
+            let otherHomeGameInfos = allTeams.find((element) =>
+              infos.TeamGames.find(
+                (teamgame) =>
+                  teamgame.TeamID === element.TeamID &&
+                  teamgame.HomeOrAway === 'HOME'
               )
-              .map(value => console.log([
-                    data.requestTwo
-                    .filter(
-                      x => x.TeamID === value.TeamID
-                    ).map(x => [
-                      x.TeamID,
-                      x.WikipediaLogoUrl,
-                      value.Wins === 1,
-                      value.HomeOrAway
-                  ])
-                ])
-              ),
-            ]
-          )
-        )
-      ).subscribe(res => [
-        this.gameList = res,
-        console.log(this.gameList),
-        console.log(res)
-      ]);
+            )?.WikipediaLogoUrl;
 
-    // this.nbaService.requestDataFromMultipleSources(currentDateTime)
-    // .subscribe(res => console.log(res));
+            let otherAwayGameInfos = allTeams.find((element) =>
+              infos.TeamGames.find(
+                (teamgame) =>
+                  teamgame.TeamID === element.TeamID &&
+                  teamgame.HomeOrAway === 'AWAY'
+              )
+            )?.WikipediaLogoUrl;
 
-    // this.nbaService.getAllTeams()
-    // .pipe(map(data => data.filter(teams => teams.Key === "NO")))
-    // .subscribe((data) => console.log(data));
+            const fullInformations: FullInformation = {
+              GameInformations: infos,
+              otherHomeGameInfos,
+              otherAwayGameInfos,
+            };
+            return fullInformations;
+          });
 
-    // this.nbaService.getMatchsOfTheDay(currentDateTime).subscribe((data) => {
-    //   // this.gameList = data;
-
-    //   this.nbaService.getAllTeams().subscribe((allTeam) => {
-    //     this.teamLogo = allTeam;
-
-    //     const teamLogoIds = this.gameList
-    //       .filter((logo) => logo.TeamGames.filter((y) => y.TeamID))
-    //       .map((logo) => logo.TeamGames.map((y) => y.TeamID));
-
-    //     const teamIds = allTeam.filter((x) =>
-    //       teamLogoIds.find((element) => element.includes(x.TeamID)));
-
-    //     const teamLogos = this.gameList
-    //       .filter((x) => x.TeamGames)
-    //       .map((y) =>
-    //         y.TeamGames.map((x) => [
-    //           x.TeamID,
-    //           ...teamIds
-    //             .filter((z) => z.TeamID === x.TeamID)
-    //             .map((w) => w.WikipediaLogoUrl),
-    //           x.Wins === 1,
-    //           x.HomeOrAway
-    //         ])
-    //       ) as unknown as any[][][];
-
-    //     this.gameList.map((x, index) => {
-    //       const awayOrHomeTeamLogo = teamLogos[index].map((y) => y[1]);
-    //       x.Game.HomeTeamLogo = awayOrHomeTeamLogo[1];
-    //       x.Game.AwayTeamLogo = awayOrHomeTeamLogo[0];
-    //       const winnerTeam = teamLogos[index].filter((y) => y[2] === true).map(y => y[3]);
-    //       x.Game.IsTheWinner = winnerTeam[0];
-    //     });
-    //   });
-    // });
+          return mergeGameInfos;
+        })
+      )
+      .subscribe((res) => [(this.gameList = res)]);
   }
 }
--- src/app/nba.service.ts
+++ src/app/nba.service.ts
@@ -1,67 +1,100 @@
-import { TeamsResponse, PlayerResponse, PlayerByTeamResponse, PlayerStats, Season, PlayerByIdResponse } from './@models/models';
+import {
+  TeamsResponse,
+  PlayerResponse,
+  PlayerByTeamResponse,
+  PlayerStats,
+  Season,
+  PlayerByIdResponse,
+} from './@models/models';
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { forkJoin, lastValueFrom, Observable } from 'rxjs';
-import { Game, GameInformations } from './@models/game';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GameInformations } from './@models/game';
 
-const params = new HttpParams().append('key', '0baeab542b0e4914ac0dd7f6908af13b');
+const params = new HttpParams().append(
+  'key',
+  '0baeab542b0e4914ac0dd7f6908af13b'
+);
 
 @Injectable()
 export class NbaService {
-
-  constructor(private http: HttpClient) {
-   }
+  constructor(private http: HttpClient) {}
 
   // Liste de toutes les équipes Américaines -- team component
   public getAllTeams(): Observable<TeamsResponse[]> {
-    return this.http.get<TeamsResponse[]>(`https://api.sportsdata.io/v3/nba/scores/json/AllTeams`, { params });
+    return this.http.get<TeamsResponse[]>(
+      `https://api.sportsdata.io/v3/nba/scores/json/AllTeams`,
+      { params }
+    );
   }
 
   // Requête pour avoir le détail d'une équipe -- teamdetail component
   public getTeamDetail(): Observable<TeamsResponse[]> {
-    return this.http.get<TeamsResponse[]>(`https://api.sportsdata.io/v3/nba/scores/json/AllTeams`, { params });
+    return this.http.get<TeamsResponse[]>(
+      `https://api.sportsdata.io/v3/nba/scores/json/AllTeams`,
+      { params }
+    );
   }
 
   // Liste des joueurs par équipe -- teamdetail component
   public getPlayerByTeam(team: String): Observable<PlayerByTeamResponse[]> {
-    return this.http.get<PlayerByTeamResponse[]>(`https://api.sportsdata.io/v3/nba/stats/json/Players/${team}`, {params});
+    return this.http.get<PlayerByTeamResponse[]>(
+      `https://api.sportsdata.io/v3/nba/stats/json/Players/${team}`,
+      { params }
+    );
   }
 
   // Liste des joueurs par équipe -- teamdetail component
   public getPlayerById(playerid: number): Observable<PlayerByIdResponse[]> {
-    return this.http.get<PlayerByIdResponse[]>(`https://api.sportsdata.io/v3/nba/scores/json/Player/${playerid}`, {params});
+    return this.http.get<PlayerByIdResponse[]>(
+      `https://api.sportsdata.io/v3/nba/scores/json/Player/${playerid}`,
+      { params }
+    );
   }
 
   // Joueur -- playerdetail component
   public getPlayerPhotoById(playerid: number): Observable<PlayerByIdResponse> {
-    return this.http.get<PlayerByIdResponse>(`https://api.sportsdata.io/v3/nba/scores/json/Player/${playerid}`, {params});
+    return this.http.get<PlayerByIdResponse>(
+      `https://api.sportsdata.io/v3/nba/scores/json/Player/${playerid}`,
+      { params }
+    );
   }
 
   // Liste de tous les joueurs de la NBA -- teamdetail component
   public getAllPlayers(): Observable<PlayerResponse[]> {
-    return this.http.get<PlayerResponse[]>(`https://api.sportsdata.io/v3/nba/scores/json/Players`, {params});
+    return this.http.get<PlayerResponse[]>(
+      `https://api.sportsdata.io/v3/nba/scores/json/Players`,
+      { params }
+    );
   }
 
   // Stats d'un joueur sur une année donnée et avec son personId -- player component
   public getSeason(): Observable<Season> {
-    return this.http.get<Season>(`https://api.sportsdata.io/v3/nba/scores/json/CurrentSeason`, {params});
+    return this.http.get<Season>(
+      `https://api.sportsdata.io/v3/nba/scores/json/CurrentSeason`,
+      { params }
+    );
   }
 
   // Stats d'un joueur sur une année donnée et avec son personId -- player component
-  public getPlayerProfile(season: number, playerid: number): Observable<PlayerStats> {
+  public getPlayerProfile(
+    season: number,
+    playerid: number
+  ): Observable<PlayerStats> {
     // tslint:disable-next-line: max-line-length
-    return this.http.get<PlayerStats>(`https://api.sportsdata.io/v3/nba/stats/json/PlayerSeasonStatsByPlayer/${season}/${playerid}`, {params});
+    return this.http.get<PlayerStats>(
+      `https://api.sportsdata.io/v3/nba/stats/json/PlayerSeasonStatsByPlayer/${season}/${playerid}`,
+      { params }
+    );
   }
 
-  public getMatchsOfTheDay(currentDateTime: string | null): Observable<GameInformations[]> {
-    return this.http.get<GameInformations[]>(`https://api.sportsdata.io/v3/nba/stats/json/BoxScores/${currentDateTime}`, {params});
+  public getMatchsOfTheDay(
+    currentDateTime: string | null
+  ): Observable<GameInformations[]> {
+    return this.http.get<GameInformations[]>(
+      `https://api.sportsdata.io/v3/nba/stats/json/BoxScores/${currentDateTime}`,
+      { params }
+    );
   }
-
-  // public requestDataFromMultipleSources(currentDateTime: string | null): Observable<any[]> {
-  //   let response1 = this.http.get<GameInformations[]>(`https://api.sportsdata.io/v3/nba/stats/json/BoxScores/${currentDateTime}`, {params});
-  //   let response2 = this.http.get<TeamsResponse[]>(`https://api.sportsdata.io/v3/nba/scores/json/AllTeams`, { params });
-  //   // Observable.forkJoin (RxJS 5) changes to just forkJoin() in RxJS 6
-  //   return forkJoin([response1, response2]);
-  // }
 }
